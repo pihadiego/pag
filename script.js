d4843b6035652fe4dfac46a6cd41f141334db75c
pihadiego <pihaclan@gmail.com>
2021-10-26 16:12:53 -0300

    .-

---
+++ script.js
@@ -0,0 +1,5 @@
+ type="text/javascript">
+        window.addEventListener("scroll", function () {
+          var header = document.querySelector("header");
+          header.classList.toggle("sticky", window.scrollY > 0);
+        });
